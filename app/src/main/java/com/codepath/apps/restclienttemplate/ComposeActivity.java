5d6c7e6bee88167508af4a041e4af03aa1f58940
EJamdar05 <jamdareshaq@gmail.com>
2022-02-20 18:26:56 -0800

    added char count to tweet compose

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -5,33 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.google.android.material.internal.TextWatcherAdapter;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
+import org.w3c.dom.Text;
 
 import okhttp3.Headers;
 
 public class ComposeActivity extends AppCompatActivity {
     private android.widget.EditText EditText;
     private Button tweetButton;
+    private TextView charsRemain;
     private String TAG = "TweetPublish";
     TwitterClient client;
+    private int totalChars = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
         EditText = findViewById(R.id.editTextTweet);
         tweetButton = findViewById(R.id.buttonTweet);
+        charsRemain = findViewById(R.id.remainingChars);
         client = TwitterApp.getRestClient(this);
         //handling tweet
+        EditText.addTextChangedListener(new TextWatcher(){
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                charsRemain.setText(280 - editable.toString().length() + "/280");
+            }
+        });
         tweetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
